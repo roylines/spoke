667d90a27d7108d9b6f5cf909c38918657afc239
Roy Lines <roy@roylines.co.uk>
2012-04-09 16:50:54 +0000

    changed process.env logic to use dot notation

---
+++ test/spokemongodb_test.js
@@ -0,0 +1,33 @@
+var assert = require('assert'),
+    mongodb = require('mongodb'),
+    spokemongodb = require('../lib/spokemongodb'),
+    vows = require('vows');
+    
+vows
+.describe('spokemongodb')
+.addBatch({  
+  'calling configuration with no variables set': {
+    topic: function() {
+      process.env = { };
+      return spokemongodb.configuration;
+    },
+    'should set default database': function(config) {
+      assert.equal(config.database, "spoke");
+    },
+    'should set default host': function(config) {
+      assert.equal(config.host, "localhost");
+    },
+    'should set default port': function(config) {
+      assert.equal(config.port, mongodb.Connection.DEFAULT_PORT);
+    },
+    'should set default user': function(config) {
+      assert.equal(config.user, undefined);
+    },
+    'should set default password': function(config) {
+      assert.equal(config.password, undefined);
+    },
+    teardown: function() {
+    }
+  }
+})
+.export(module);
